01efb7eecadd6113319b931a139e7a747110cc3d
murodjonrabbimov7-glitch <murodjonrabbimov7@gmail.com>
2026-02-26 20:00:19 +0500

    Update image URL in App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/hero.png" alt="Hero" />  {/* Updated image URL */}
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
